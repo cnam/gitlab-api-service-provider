2992302062245d64a047ae79eb2cf760254225a2
cnam <cnam812@gmail.com>
2014-08-16 18:04:44 +0400

    firs alpha release

---
+++ src/Silex/Provider/GitlabApiSilexProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Silex\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Gitlab\Api;
+
+class GitlabApiSilexProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['gitlab_api.default_options'] = array(
+            "verify" => false,
+            'auth' => array('user', 'pass'),
+            'query' => array(
+                'private_token' => 'YOU_PRIVATE_TOKEN'
+            )
+        );
+
+        $app['gitlab_api'] = function ($app) {
+            return new Api(array(
+                'request.options' => array(
+                    "verify" => false,
+                    "auth" => $app['gitlab_api.request_options']['auth'],
+                    "query" => array(
+                        'private_token' => $app['gitlab_api.request_options']['private_token']
+                    )
+                ))
+            );
+        };
+    }
+}
