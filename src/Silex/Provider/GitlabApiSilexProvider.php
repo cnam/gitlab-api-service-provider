f8cb65750d794c6c2329a919ee84440c511b7226
cnam <cnam812@gmail.com>
2014-08-30 13:51:07 +0400

    fixed user providers for multi users

--- src/Silex/Provider/GitlabApiSilexProvider.php
+++ src/Silex/Provider/GitlabApiSilexProvider.php
@@ -14,20 +14,29 @@ class GitlabApiSilexProvider implements ServiceProviderInterface
             "verify" => false,
             'auth' => array('user', 'pass'),
             'query' => array(
-                'private_token' => 'YOU_PRIVATE_TOKEN'
+                'private_token' => 'You_Private_token'
             )
         );
 
         $app['gitlab_api'] = function ($app) {
+
+            $token = $app['security']->getToken();
+            if (null !== $token) {
+                $user = $token->getUser();
+                $private_token = $user->getToken();
+            } else {
+                $private_token = $app['gitlab_api.request_options']['private_token'];
+            }
+
             return new Api(array(
-                "base_url" => $app['gitlab_api.base_url'],
-                'request.options' => array(
-                    "verify" => false,
-                    "auth" => $app['gitlab_api.request_options']['auth'],
-                    "query" => array(
-                        'private_token' => $app['gitlab_api.request_options']['private_token']
-                    )
-                ))
+                    "base_url" => $app['gitlab_api.base_url'],
+                    'request.options' => array(
+                        "verify" => false,
+                        "auth" => $app['gitlab_api.request_options']['auth'],
+                        "query" => array(
+                            'private_token' => $private_token
+                        )
+                    ))
             );
         };
     }
